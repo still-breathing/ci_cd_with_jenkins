cd14c01bb876e53959689090c23211a29347eec8
raiyeemfarhan <raiyeem@cefalo.com>
2025-10-07 13:31:15 +0600

    revert back to old example

--- app.py
+++ app.py
@@ -1,4 +1,23 @@
 #!/usr/bin/env python3
-print("Start/n")
-print("This is used for learning ci cd with jenkins & github")
-print("/nEnd")
+
+
+def add(a, b):
+    return a + b
+
+if __name__ == "__main__":
+    print(add(2, 3))
+
+
+'''
+def message():
+    print("Start\n")
+    print("This is used for learning ci cd with jenkins & github")
+    print("\nEnd")
+
+if __name__ == "__main__":
+    message()
+'''
+
+
+
+

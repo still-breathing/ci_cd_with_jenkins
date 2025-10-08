d7ab090d1cf756a66f172ebda757d5687587c69e
raiyeemfarhan <raiyeem@cefalo.com>
2025-10-09 04:21:01 +0600

    trying to fix tests for jenkins

--- tests/test_initial.py
+++ tests/test_initial.py
@@ -1,8 +1,18 @@
 
 
+# from app import add
+
+# def test_add():
+#     print("start of test")
+#     assert add(3,4) == 7
+#     print("end of test")
+
+# tests/test_initial.py
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from app import add
 
-def test_add():
-    print("start of test")
-    assert add(3,4) == 7
-    print("end of test")
+def test_add_numbers():
+    assert add(3, 4) == 7
